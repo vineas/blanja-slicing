a3617f363ded235ec025b986dbe12420e16730d0
vineas <alvienasyandika@gmail.com>
2023-07-03 03:16:37 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.btn-customer-pills');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'salmon';
+  btn.style.color = 'white';
+});
